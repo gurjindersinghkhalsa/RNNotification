6ace3767f74c2f4c04b2c729ce87c6111d8864c1
gurjindersingh <gurjindersingh663@gmail.com>
2022-05-20 18:32:38 +0530

    initial project setup

---
+++ RNNotification/App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Notifications} from 'react-native-notifications';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App() {
+  Notifications.registerRemoteNotifications();
+
+    Notifications.events().registerNotificationReceivedForeground((notification: Notification, completion) => {
+      console.log(`Notification received in foreground: ${notification.title} : ${notification.body}`);
+      completion({alert: false, sound: false, badge: false});
+    });
+
+    Notifications.events().registerNotificationOpened((notification: Notification, completion) => {
+      console.log(`Notification opened: ${notification.payload}`);
+      completion();
+    });
+  return(
+    <View style={{ flex: 1}}>
+      <Text>Push Notification</Text>
+    </View>
+  );
+}
+
+export default App;
